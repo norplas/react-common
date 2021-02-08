b5c4770c5112895ebc18f913462303a6c01d10bb
Charles Berman <Charles.Berman@magna.com>
2021-02-08 10:02:43 -0500

    update options for VersionNotifier

--- src/components/VersionNotifier.tsx
+++ src/components/VersionNotifier.tsx
@@ -2,22 +2,37 @@ import React from 'react';
 import { SnackbarProvider, useSnackbar } from 'notistack';
 import useVersion from './useVersion';
 
+
+const getNotifyOptions=(options:any)=>{
+    
+    let result = {
+        persist:options.persists||true,
+        preventDuplicate:options.preventDuplicate||true,
+        variant:options.variant||'error',
+        anchorOrigin:{
+            vertical:options.vertical||'top',
+            horizontal:options.center||'center'
+        }
+    };
+
+  return result;
+}
  
 
 const Content = ({ name, children,...otherProps }: any) => {
     const { enqueueSnackbar } = useSnackbar();
 
+
+
+
+    let options = getNotifyOptions(otherProps);
+
+    console.log(options);
     const handleUpdate = (value: String) => {
 
-    
-        enqueueSnackbar(`There is a new version of ${value}, please refresh your browser`, {
-            persist:true,
-            preventDuplicate:true,
-            anchorOrigin:{
-                vertical:'top',
-                horizontal:'center'
-            }
-        });
+        
+
+        enqueueSnackbar(`There is a new version of ${value}, please refresh your browser`, options);
     }
 
     useVersion(handleUpdate, name);
