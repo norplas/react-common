2950e96b39bbe44b638e60cca27f3058877814ab
Charles Berman <Charles.Berman@magna.com>
2021-02-20 21:09:39 -0500

    fixed the logo

---
+++ src/stories/Logo.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import LogoComponent, { LogoType } from '../components/Logo';
+
+export default {
+  title: 'Example/Logo',
+  component: LogoComponent,
+  argTypes:{
+      color: {control:'color'}
+  }
+} as Meta;
+
+const Template: Story<LogoType> = (args) => <LogoComponent {...args} />;
+
+export const Logo = Template.bind({});
+Logo.args = {
+  color:'#FF00FF'
+};
+ 
